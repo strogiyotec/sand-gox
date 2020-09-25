60978e40f0f27ec72065c4e7e2c7e746be8ac892
strogiyotec <almas337519@gmail.com>
2020-09-25 09:56:39 -0700

    permutations

--- leetcode/permutations/main.go
+++ leetcode/permutations/main.go
@@ -9,22 +9,26 @@ func main() {
 func permute(nums []int) [][]int {
 	solution := [][]int{}
 	comb := []int{}
-	return permutations(&nums, 0, len(nums)-1, &solution, &comb)
+	index := 0
+	permutations(&nums, 0, len(nums)-1, &solution, &comb, &index)
+	return solution
 }
 
-func permutations(nums *[]int, from int, end int, solution *[][]int, permutation *[]int) {
+func permutations(nums *[]int, from int, end int, solution *[][]int, permutation *[]int, index *int) {
 	if from == end {
-		append(solution, permutation)
+		tmp := make([]int, len(*nums))
+		copy(tmp, *nums)
+		*solution = append(*solution, tmp)
 	}
 	for i := from; i <= end; i++ {
 		swap(nums, from, i)
-		permutations(nums, from+1, end, solution, permutation)
+		permutations(nums, from+1, end, solution, permutation, index)
 		swap(nums, from, i)
 	}
 }
 
 func swap(nums *[]int, from int, to int) {
-	temp := from
-	nums[from] = to
-	nums[to] = temp
+	temp := (*nums)[from]
+	(*nums)[from] = (*nums)[to]
+	(*nums)[to] = temp
 }
